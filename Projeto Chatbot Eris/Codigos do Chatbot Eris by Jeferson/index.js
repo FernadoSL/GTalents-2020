c7ef36794d508a8a31b93ff647c45ee24fdf8e0d
Jeffolivera <jeffolivera01@gmail.com>
2020-11-26 16:50:29 -0300

    Correção de códigos 1.2

--- Projeto Chatbot Eris/Codigos do Chatbot Eris by Jeferson/index.js
+++ Projeto Chatbot Eris/Codigos do Chatbot Eris by Jeferson/index.js
@@ -2,13 +2,13 @@
 import json from 'body-parser';
 import express from 'express';
 import ClienteServico from "./clienteServico.js";
-import Cliente from "./cliente.js" 
+import Cliente from "./cliente.js"
+import Mercado from "./mercado.js"
 
 const app = express();
 app.use(json());
 
 var clienteServico = new ClienteServico();
-var cliente = new Cliente();
 
 app.get('/webhook', (request, response) => {
 
@@ -26,18 +26,13 @@ app.post('/webhook', (request, response) => {
     // intents
     var nomeIntencao = data.queryResult.intent.displayName;
 
-    
+    console.log(clienteServico.loginFeito);
     //intent carrinho de compras
-    if(nomeIntencao == 'IntentListaCarrinho' && clienteServico.clienteLogado){
-        //quando o usuário digitar lista de compras
+    if(nomeIntencao == 'IntentListaCarrinho' && clienteServico.loginFeito){
         
-        var parametros = data.queryResult.parameters;
-        parametros = 'listaCompras';
-
-        var listaCompras = clienteServico.clienteLogado.listaCompras;
         var listaString = "";
-        for (let i = 0; i < cliente.listaCompras.length; i++) {
-            listaString = listaCompras + ", ";
+        for (let i = 0; i < clienteServico.clienteLogado.listaCompras.length; i++) {
+            listaString = listaString + clienteServico.clienteLogado.listaCompras[i].name + ", ";
         }
 
         var responseData =
@@ -46,32 +41,55 @@ app.post('/webhook', (request, response) => {
             };
         response.json(responseData)
     
-    }else if(nomeIntencao == 'IntentListaCarrinho' && !clienteServico.clienteLogado){
-    
+    }else if(nomeIntencao == 'IntentListaCarrinho' && !clienteServico.loginFeito){
+        var parametros = data.queryResult.parameters;
+        parametros = 'listaCompras';
+
         var responseData =
             {
                 fulfillmentMessages: [{ text: { text: ["Para acessar sua lista de compras digite seu CPF."] } }]
             };
         response.json(responseData)
     }
-    console.log(clienteServico.clienteLogado);
 
     //intent que verifica se o usuário é cadastrado ou não
     if(nomeIntencao == 'IntentcheckCPF'){
 
-        var cpf = data.queryResult.queryText.parameters;
+        var cpf = data.queryResult.parameters.number;
         clienteServico.checkCliente(cpf);
+        console.log(cpf);
+        var responseData =
+            {
+                fulfillmentMessages: [{ text: { text: ["Seus dados foram registrados com sucesso! Para acessar sua lista de compras novamente digite 'minha lista de compras'."] } }]
+            };
+        response.json(responseData)
     }
 
+
     if(nomeIntencao == 'IntentAdicionaCarrinho' && clienteServico.loginFeito){
-        clienteServico.clienteLogado.addCliente(name,price);
+        var addItensCarrinho = data.queryResult.parameters.Produto;
+
+        clienteServico.clienteLogado.addCarrinho(addItensCarrinho);
+        var responseData =
+            {
+                fulfillmentMessages: [{ text: { text: ["Item adicionado na lista de compras"] } }]
+            };
+        response.json(responseData)
+
     }
     
 
 })
 
 app.delete('/webhook', (request, response) => {
+    var data = request.body;
 
+    if(nomeIntencao == 'IntentDeletadoCarrinho'){
+        var deleteItensCarrinho = data.queryResult.queryText.parameters;
+        var deletarNome = deleteItensCarrinho.name;
+        var deletarPreco = deleteItensCarrinho.price;
+        cliente.deleteCarrinho(deletarNome, deletarPreco);
+    }
 })
 
 
